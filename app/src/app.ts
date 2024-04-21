01ff64b2a3a2818d1a3bf6c3cf7471a33c4bc072
AndreLuiz2022 <andreluiz01bhz@gmail.com>
2024-04-20 22:06:08 -0300

    testando o card com o botao de ver modulos

--- app/src/app.ts
+++ app/src/app.ts
@@ -20,23 +20,25 @@ app.use(morgan("dev"));
 
 app.post("/", (req: Request, res: Response) => {
   const cursos = {
-    tecnologia : `Em tecnologia, as unidades de BH da Proz ofertam o curso técnico em Desenvolvimento de Sistemas`,
+    tecnologia: `Em tecnologia, as unidades de BH da Proz ofertam o curso técnico em Desenvolvimento de Sistemas`,
     gestao: `Em gestão, as unidades de BH da Proz ofertam os seguintes cursos técnicos: \n${bullet} Administração; \n Recursos Humanos; \n Segurança do Trabalho; \n Logística`,
-    saude: `Em saúde, as unidades de BH da Proz ofertam os seguintes cursos técnicos: \n${bullet} Enfermagem; \n${bullet} Radiologia; \n${bullet} Estética;`
-  }
+    saude: `Em saúde, as unidades de BH da Proz ofertam os seguintes cursos técnicos: \n${bullet} Enfermagem; \n${bullet} Radiologia; \n${bullet} Estética;`,
+  };
 
   const cursosInfo = {
-    "ads" : "O Técnico em Desenvolviemnto de Sistemas possui uma carga horária de 1200 horas, equivalentes a 2 anos de aulas 20% online e 80% presenciais, com 8 módulos, 16 disciplinas. \nEsses módulos são: \n${bullet} Tecnologia da informação; \n${bullet} Programação de Sistemas; \n${bullet} Análise de Sitemas; \n${bullet} Desenvolvimento de Sistemas para celulares; \n${bullet} Programação Front-end; \n${bullet} Programação back-end; \n${bullet} Administração de Redes; \n${bullet} Qualidade de Software.",
+    ads: {
+      title: "Técnico em Desenvolvimento de Sistemas",
+      info: `O Técnico em Desenvolviemnto de Sistemas possui uma carga horária de 1200 horas, equivalentes a 2 anos de aulas 20% online e 80% presenciais, com 8 módulos, 16 disciplinas.`,
+      modulos: `Os módulos são: \n${bullet} Tecnologia da informação; \n${bullet} Programação de Sistemas; \n${bullet} Análise de Sitemas; \n${bullet} Desenvolvimento de Sistemas para celulares; \n${bullet}  Programação Front-end; \n${bullet} Programação back-end; \n${bullet} Administração de Redes; \n${bullet} Qualidade de Software.`,
+    },
 
-    "enf" : "O Técnico em Enfermagem possui uma carga horária de 1680 horas, equivalentes a 2 anos de aulas 20% online e 80% presenciais, com 8 módulos, 16 disciplinas. Dessa carga horária, 480 horas correspondem a estágios obrigatórios para a formação do aluno.\nOs módulos presentes são: \nEnfermagem na atenção à saúde do adulto, Enfermagem na atenção domiciliar, Enfermagem na clínica cirúrgica, Enfermagem na saúde coletiva, Enfermagem na saúde da mulher, do homem, da criança e do adolescente, Assistência ao paciente crítico adulto, Assistência ao paciente crítico neonatal e pediátrico e Enfermagem na qualidade e segurança do paciente."
-
-    
-  }
+    enf: "O Técnico em Enfermagem possui uma carga horária de 1680 horas, equivalentes a 2 anos de aulas 20% online e 80% presenciais, com 8 módulos, 16 disciplinas. Dessa carga horária, 480 horas correspondem a estágios obrigatórios para a formação do aluno.\nOs módulos presentes são: \nEnfermagem na atenção à saúde do adulto, Enfermagem na atenção domiciliar, Enfermagem na clínica cirúrgica, Enfermagem na saúde coletiva, Enfermagem na saúde da mulher, do homem, da criança e do adolescente, Assistência ao paciente crítico adulto, Assistência ao paciente crítico neonatal e pediátrico e Enfermagem na qualidade e segurança do paciente.",
+  };
 
   console.log(req.body);
   const agent = new WebhookClient({ request: req, response: res });
 
-  function displayCursosDireta( agent: WebhookClient) {
+  function displayCursosDireta(agent: WebhookClient) {
     const areasCursos = agent.parameters["areas-cursos"];
     agent.add(cursos[areasCursos]);
   }
@@ -55,19 +57,18 @@ app.post("/", (req: Request, res: Response) => {
   function displayCursoInfo(agent: WebhookClient) {
     const curso = agent.parameters["cursos"];
 
-    if(!curso) {
-      agent.add("Me diga sobre qual curso você deseja mais informações")
+    if (!curso) {
+      agent.add("Me diga sobre qual curso você deseja mais informações");
     } else {
-      agent.add(cursosInfo[curso]);
+      agent.add(
+        new Card({
+          title: cursosInfo[curso]["title"],
+          text: cursosInfo[curso]["info"],
+          buttonText: "Ver módulos",
+          buttonUrl: `quero ver os modulos do curso de ${curso}`,
+        })
+      );
     }
-
-    agent.add(new Card({
-      title : "Titulo teste",
-      text : "lorem lorem lorem lorem lorem lorem lorem",
-      subtitle: "teste",
-      buttonText : "voltar para menu",
-      buttonUrl : "menu"
-    }));
   }
 
   let intentMap = new Map();
